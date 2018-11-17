62fce8f47432a34d2c3dd16302eb5eeac5de877f
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2018-11-16 22:34:14 -0200

    update 48k

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -5,7 +5,7 @@ class Welcome extends Component {
     return (
       <div className="Welcome animated fadeIn">
         <h1 className="App-title">Vue Theme</h1>
-        <p className="Installs">+46700 Installs</p>
+        <p className="Installs">+48000 Installs</p>
         <p className="App-intro">
           Theme for Visual Studio Code inspired by Vue.js
         </p>
