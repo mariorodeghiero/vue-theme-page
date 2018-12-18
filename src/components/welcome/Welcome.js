a7c35ae28b9dfcaca6580425e8043cf3e7e8b1b4
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2018-12-18 19:34:27 -0200

    change: update downloads

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Welcome extends Component {
   render() {
     return (
       <div className="Welcome animated fadeIn">
         <h1 className="App-title">Vue Theme</h1>
-        <p className="Installs">+48000 Installs</p>
+        <p className="Installs">+62000 Downloads</p>
         <p className="App-intro">
           Theme for Visual Studio Code inspired by Vue.js
         </p>
